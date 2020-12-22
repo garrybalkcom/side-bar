b55b4f63e304e0c227e71bb8e1d446b0d325e41e
garrybalkcom <garry.balkcom@live.com>
2020-12-22 09:39:34 -0500

    first draft

---
+++ main.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+};
+function closeMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+}
